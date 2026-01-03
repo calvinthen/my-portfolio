2d5ddad74fe8b6c1ffd8ccd20ce7da92dc1a6ebd
calvinthen <samkurniawan1st@gmail.com>
2026-01-03 14:28:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        DevFolio: Under Construction
+      </h1>
+    </div>
+  );
+}
+
+export default App;
