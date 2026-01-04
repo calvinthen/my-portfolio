abf9a29d33c8292471e0fccb3ad92af16ffd5480
calvinthen <samkurniawan1st@gmail.com>
2026-01-04 21:04:12 +0700

    add css to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
-    <nav className="">
-        <div className="">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-slate-900/90 backdrop-blur-sm border-b border-slate-800">
+        <div className="container mx-auto px-6 py-4 flex justify-between items-center">
             <div>
                 Dev<span className="text-white">Folio</span> 
             </div>
