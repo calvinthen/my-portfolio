e5887dde329f25a8f78309c4f4ee22cc5ff37879
calvinthen <samkurniawan1st@gmail.com>
2026-01-05 17:30:36 +0700

    hero section preparation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
     <div className="bg-slate-900 min-h-screen text-white flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-500">
         <Navbar/>
-        
+        <Hero/>
       </h1>
     </div>
   );
