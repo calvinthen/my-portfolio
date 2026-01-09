8d8c9de73184a49694e30a95fa5dd33e7aafd196
calvinthen <samkurniawan1st@gmail.com>
2026-01-09 14:46:08 +0700

    set hook use state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-slate-900/90 backdrop-blur-sm border-b border-slate-800">
         <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-            <div className="text-2xl">
+            <div className="text-2xl text-blue-500 font-bold">
                 Dev<span className="text-white">Folio</span> 
             </div>
 
@@ -13,8 +18,13 @@ const Navbar = () => {
                 <a href="#contact" className="hover:text-blue-500 transition"> Contact</a>
             </div>
 
-            <button className="md:hidden text-white focus:outline-none">
-                Menu
+            <button className="md:hidden text-white focus:outline-none"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}>
+               {isMenuOpen ? (
+                <svg></svg>
+                ) : (
+                   <svg></svg>
+               )}
             </button>
 
         </div>
