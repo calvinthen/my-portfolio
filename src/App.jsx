e7a21b876939afb957a2a3d121c427507cb4de03
calvinthen <samkurniawan1st@gmail.com>
2026-01-11 16:29:26 +0700

    prepraring contact.jsx work file and skeleton html

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
 import Projects from "./components/Projects";
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+const Contact = () => {
+  return (
+    <section id="contact">
+        <div>
+            <div>
+                <h2>
+                    Get In <span className="text-blue-500"> Touch</span>
+                </h2>
+            </div>
+
+        </div>
+    </section>
+  );
+};
+
+export default Contact;
