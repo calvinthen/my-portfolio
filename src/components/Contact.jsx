d9a6bc286acea857c1bbd9f5e189260f3a1e6574
calvinthen <samkurniawan1st@gmail.com>
2026-01-13 19:52:44 +0700

    complete skeleton html for contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,42 @@
 const Contact = () => {
   return (
-    <section id="contact">
-        <div>
-            <div>
+    <section id="contact" className="py-20 bg-slate-900">
+        <div className="container mx-auto px-6">
+            <div className="max-w-2xl mx-auto text-center">
                 <h2>
                     Get In <span className="text-blue-500"> Touch</span>
                 </h2>
-            </div>
 
+                <p>
+                   I'm currently looking for new opportunities. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+                </p>
+            
+
+                <form action="">
+                    <div>
+                        <label htmlFor="">
+                            Your Email
+                        </label>
+                        <input 
+                        type="text" 
+                        />
+
+                    </div>
+
+                    <div>
+                        <label htmlFor="">
+                            Message
+                        </label>
+                        <textarea name="" id="">
+
+                        </textarea>
+                    </div>
+
+                    <button>
+                        Send Message
+                    </button>
+                </form>
+            </div>
         </div>
     </section>
   );
