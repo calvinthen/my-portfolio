19ff04602f308afb0d9bab937026a57f7f7772b7
calvinthen <samkurniawan1st@gmail.com>
2026-01-20 18:59:20 +0700

    add footer skeleton html

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
 import Projects from "./components/Projects";
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Projects />
       <Contact />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="">
+        <p>
+            © {new Date().getFullYear()} Calvin Then. All rights reserved.
+        </p>
+        <div>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
