549a1a0dd036d6e3eaa7f276f20794c1c9bb15d2
calvinthen <samkurniawan1st@gmail.com>
2026-01-25 21:23:41 +0700

    fix: capitalization of Navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import ScrollToTop from "./components/ScrollToTop";
 
